c9e3e446a04310675ada1f9d09c3978adaa920b2
Owen <owencasabuena@gmail.com>
2026-06-05 09:50:14 +0800

    feat: beat recursion

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,24 @@
-const permutations = function() {
-  
+const permutations = function(set) {
+    const results = [];
+    
+    function backtrack(currentPath, remainingOptions) {
+        if (remainingOptions.length < 1) {
+            results.push(currentPath);
+        }
+        
+        for (let i = 0; i < remainingOptions.length; i++) {
+            let currentItem = remainingOptions[i];
+
+            let nextOptions = remainingOptions.slice(0, i).concat(remainingOptions.slice(i + 1));
+            let nextPath = [...currentPath, currentItem];
+
+            backtrack(nextPath, nextOptions);
+        
+        }
+    }
+    backtrack([], set);
+
+    return results;
 };
   
 // Do not edit below this line
